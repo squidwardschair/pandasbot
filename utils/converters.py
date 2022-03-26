eb771573962c7a34db579466d7c1ac2ea91fe5fe
squidwardschair <hudsonzhao1@gmail.com>
2022-03-26 10:37:18 -0400

    bgdshghsg

--- utils/converters.py
+++ utils/converters.py
@@ -11,7 +11,7 @@ time_dict = {"h": 3600, "s": 1, "m": 60, "d": 86400}
 
 class TimeConverter(commands.Converter):
     async def convert(self, ctx: commands.Context, argument: str):
-        if argument.endswith('s') or argument.endswith('h') or argument.endswith('m') or argument.endswith('d'):
+        if (argument.endswith('s') or argument.endswith('h') or argument.endswith('m') or argument.endswith('d')) and (not argument.endswith('pm') or not argument.endswith('am')):
             get_unit = argument[-1]
             if get_unit in time_dict:
                 return time_dict[get_unit] * int(argument.rstrip(get_unit))
